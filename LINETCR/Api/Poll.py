34f8678913fcebe6f66110667975e5f8d422d16a
99tonza99 <33803741+99tonza99@users.noreply.github.com>
2017-12-15 20:34:16 +0700

    J

--- LINETCR/Api/Poll.py
+++ LINETCR/Api/Poll.py
@@ -19,7 +19,7 @@ class Poll:
   port = 443;
   
   UA = "Line/7.14.0 IOS\t7.14.0\tiPhone OS\t10.12.0"
-  US = "Line/7.14.0 IOS\t7.14.0\tiPhone OS\t10.12.0"
+  LA = "Line/7.14.0 IOS\t7.14.0\tiPhone OS\t10.12.0"
   
 
   rev = 0
